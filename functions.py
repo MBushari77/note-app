ce2f5fdff2f31c233457e7a2cdd58efddff20c7a
Mohammed El-Bushari <40209149+MBushari77@users.noreply.github.com>
2020-07-05 22:46:57 -1200

    second commit

--- functions.py
+++ functions.py
@@ -1,27 +1,9 @@
 import os
 import tempfile
+from model import *
 
-
-# HERD PRINTING FUNCTION
-def toPrinter(x):
-	filename = tempfile.mktemp('.txt')
-	open(filename, "w").write(str(x))
-	os.startfile(filename, 'print')
-
-
-
-# GET THE NUMBER OF DUPLICATED NUMBERS IN A LIST
-def getRepPro(listOfElems):
-	dictOfElems = dict()
-	print(" ")
-	print(" ")
-	print(" ")
-	print(listOfElems)
-	for elem in listOfElems:
-		if elem in dictOfElems:
-			dictOfElems[elem] += 1
-		else:
-			dictOfElems[elem] = 1
-	dictOfElems = { key:{'times':value, 'price': 0, 'name': 'none', 'id': key, 'pices': 0, 'costPBC': 0, 'ppp': 0, 'totPs': 0} for key, value in dictOfElems.items() if value > 0}
-
-	return dictOfElems
+def jsonNotes(peweObj):
+	notes = []
+	for note in peweObj:
+		notes.append({'title': note.title, 'content': note.content, 'date': note.date, 'ownerId': note.ownerId, 'id': note.id})
+	return notes
